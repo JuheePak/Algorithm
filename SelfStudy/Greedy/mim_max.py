ab4f316679827ce012501063f53ddc68500ff0f9
Juhee Pak <juhee.pak.06@gmail.com>
2021-01-09 12:53:17 +0900

    Add min_max.py

---
+++ SelfStudy/Greedy/mim_max.py
@@ -0,0 +1,17 @@
+# min, max 함수 연습
+
+# 보통 자주 쓰는 형식의 min, max
+print(min(1, 2, 3))
+print(max(1, 2, 3))
+
+# exam3에서 쓰인 min, max 방식. 변수끼리 비교한다.
+a = 1000
+b = 2000
+print(min(a, b))
+print(max(a, b))
+
+minVal = min(a, b)
+print(minVal)
+maxVal = max(a, b)
+print(maxVal)
+
