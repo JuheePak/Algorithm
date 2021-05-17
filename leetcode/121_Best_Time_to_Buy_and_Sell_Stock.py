e3ca66801da9ca02af88b5f3d2d3fa376c79cd80
Juhee Pak <juhee.pak.06@gmail.com>
2021-05-17 21:31:35 +0900

    Update README.md

---
+++ leetcode/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -0,0 +1,22 @@
+def maxProfit(prices):
+    minNum = 9999
+    for i in prices:
+        if minNum > i:
+            minNum = i
+
+    idx = prices.index(minNum)
+    maxNum = -1
+    for i in range(idx+1, len(prices)):
+        if maxNum < prices[i]:
+            maxNum = prices[i]
+
+    result = maxNum-minNum
+    if result > 0:
+        return result
+    else:
+        return 0
+
+#prices = [7,3,1,5,3,6,4]
+prices = [7, 3, 1]
+
+print(maxProfit(prices))
