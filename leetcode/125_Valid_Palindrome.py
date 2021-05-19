a8e4e78bf2c6e6672d2bcf7211a01868a903655a
Juhee Pak <juhee.pak.06@gmail.com>
2021-05-19 12:15:47 +0900

    Update 125.py

--- leetcode/125_Valid_Palindrome.py
+++ leetcode/125_Valid_Palindrome.py
@@ -1,19 +1,26 @@
-def isPalindrome(s):
+def isPalindrome(s) -> bool:
     tmp = []
     for i in s:
-        if 97 <= ord(i) < 123:
+        if 97 <= ord(i) < 123:  # 소문자
             tmp.append(i)
-        elif 65 <= ord(i) <= 90:
+        elif 65 <= ord(i) <= 90:  # 대문자
             tmp.append(i.lower())
+        elif 48 <= ord(i) <= 57:  # 숫자
+            tmp.append(i)
 
+    checkList = []
     for k in range(0, len(tmp)):
-        if tmp[k] != tmp[-k]:
-            return False
-        else:
-            return True
+        if tmp[k] != tmp[-(k + 1)]:
+            checkList.append(tmp[k])
 
+    if len(checkList) >= 1:
+        return False
+    else:
+        return True
 
 s = ["A man, a plan, a canal: Panama"]
+ss = ["race a car"]
 print(isPalindrome(s))
+print(isPalindrome(ss))
 
-# TypeError: ord() expected a character, but string of length 30 found
+
